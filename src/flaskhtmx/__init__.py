3e83dc06e285653cfede574bbfcedc3aa67af927
keroken <kenichi.shinagawa@gmail.com>
2024-02-12 00:12:54 +0900

    add entries input to render_template

--- src/flaskhtmx/__init__.py
+++ src/flaskhtmx/__init__.py
@@ -1,7 +1,26 @@
-from flask import Flask
+import jinja_partials
+import feedparser
+from flask import Flask, render_template
 
-app = Flask(__name__)
+feed = {
+  "title": "The Teclad Blog",
+  "href": "https://blog.teclado.com/rss",
+  "show_images": True,
+  "entries": {}
+}
 
-@app.route("/")
-def home():
-  return "Hello World!"
+def create_app():
+  app = Flask(__name__)
+  jinja_partials.register_extensions(app)
+
+  @app.route('/feed/')
+  def render_feed():
+    feed_url = feed["href"]
+    parsed_feed = feedparser.parse(feed_url)
+    for entry in parsed_feed.entries:
+      if entry.link not in feed["entries"]:
+        feed["entries"][entry.link] = entry
+
+    return render_template("feed.html", feed=feed, entries=feed["entries"].values())
+
+  return app
